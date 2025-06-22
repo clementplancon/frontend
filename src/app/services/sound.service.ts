c3472ae6558fafaca58da42d421763184391fbef
clementplancon <clement.plancon.pro@gmail.com>
2025-06-22 15:09:12 +0200

    fix: correct sound file paths in SoundService

--- src/app/services/sound.service.ts
+++ src/app/services/sound.service.ts
@@ -5,8 +5,8 @@ export class SoundService {
   play(sound: 'blinds-up' | 'eliminated') {
     let path = '';
     switch (sound) {
-      case 'blinds-up': path = 'assets/sounds/blinds-up.mp3'; break;
-      case 'eliminated': path = 'assets/sounds/eliminated.mp3'; break;
+      case 'blinds-up': path = 'assets/blinds-up.mp3'; break;
+      case 'eliminated': path = 'assets/eliminated.mp3'; break;
     }
     const audio = new Audio(path);
     audio.play();
